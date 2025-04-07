22cd44496cc8776a3844746228388dc08f0f3f82
GreattitJY <great.tit.jaejae@gmail.com>
2025-04-07 13:43:15 +0900

    add: customOverlayMap

--- src/app/_components/dong-polygon.tsx
+++ src/app/_components/dong-polygon.tsx
@@ -1,4 +1,4 @@
-import { Polygon } from "react-kakao-maps-sdk";
+import { Polygon, CustomOverlayMap } from "react-kakao-maps-sdk";
 import { transformer } from "@/utils/proj-transformer";
 import { GeoFeature } from "@/types/geo";
 import { useState } from "react";
@@ -18,6 +18,21 @@ export default function DongPolygon({ geoList }: DongPolygonProps) {
     setMouseOverGeo(null);
   };
 
+  const calculateCenter = (coordinates: { lat: number; lng: number }[]) => {
+    const latValues = coordinates.map((coord) => coord.lat);
+    const lngValues = coordinates.map((coord) => coord.lng);
+
+    const minLat = Math.min(...latValues);
+    const maxLat = Math.max(...latValues);
+    const minLng = Math.min(...lngValues);
+    const maxLng = Math.max(...lngValues);
+
+    return {
+      lat: (minLat + maxLat) / 2,
+      lng: (minLng + maxLng) / 2,
+    };
+  };
+
   return (
     <>
       {geoList.map((geo, i) => {
@@ -25,21 +40,27 @@ export default function DongPolygon({ geoList }: DongPolygonProps) {
           const [x, y] = transformer.forward(coord);
           return { lat: y, lng: x };
         });
+        const center = calculateCenter(coordinates);
 
         return (
-          <Polygon
-            key={i}
-            path={coordinates}
-            strokeWeight={3}
-            strokeColor="#2A7EDE"
-            strokeOpacity={0.8}
-            strokeStyle={"solid"}
-            fillColor={mouseOverGeo === geo.properties.KOR_NM ? "#EFFFED" : "#99CAFF"}
-            fillOpacity={mouseOverGeo === geo.properties.KOR_NM ? 0.8 : 0.7}
-            onMouseover={() => handleMouseOver(geo.properties.KOR_NM)}
-            onMouseout={handleMouseOut}
-            onClick={() => console.log(geo.properties.KOR_NM)}
-          />
+          <>
+            <Polygon
+              key={i}
+              path={coordinates}
+              strokeWeight={3}
+              strokeColor="#2A7EDE"
+              strokeOpacity={0.8}
+              strokeStyle={"solid"}
+              fillColor={mouseOverGeo === geo.properties.KOR_NM ? "#EFFFED" : "#99CAFF"}
+              fillOpacity={mouseOverGeo === geo.properties.KOR_NM ? 0.8 : 0.7}
+              onMouseover={() => handleMouseOver(geo.properties.KOR_NM)}
+              onMouseout={handleMouseOut}
+              onClick={() => console.log(geo.properties.KOR_NM)}
+            />
+            <CustomOverlayMap key={`marker-${i}`} position={center}>
+              <p className="rounded bg-white p-1 text-sm">{geo.properties.KOR_NM}</p>
+            </CustomOverlayMap>
+          </>
         );
       })}
     </>
